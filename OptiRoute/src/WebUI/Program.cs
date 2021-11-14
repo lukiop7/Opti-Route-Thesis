96805880df3abef74fc638f17c623a6c3564e2b0
Łukasz Kwiecień <kwiecien.lukasz@interia.pl>
2021-11-14 23:40:29 +0100

    feat: changed DB to PostgreSQL

--- OptiRoute/src/WebUI/Program.cs
+++ OptiRoute/src/WebUI/Program.cs
@@ -25,7 +25,7 @@ namespace OptiRoute.WebUI
                 {
                     var context = services.GetRequiredService<ApplicationDbContext>();
 
-                    if (context.Database.IsSqlServer())
+                    if (context.Database.IsNpgsql())
                     {
                         context.Database.Migrate();
                     }                   
