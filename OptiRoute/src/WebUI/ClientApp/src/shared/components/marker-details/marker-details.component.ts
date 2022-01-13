ddfce070ed6d77c6244d132a864a150258cb4baf
Łukasz Kwiecień <kwiecien.lukasz@interia.pl>
2022-01-13 21:29:07 +0100

    feat: customers dates validation depending on the depot

--- OptiRoute/src/WebUI/ClientApp/src/shared/components/marker-details/marker-details.component.ts
+++ OptiRoute/src/WebUI/ClientApp/src/shared/components/marker-details/marker-details.component.ts
@@ -1,38 +1,50 @@
-import {Component, Input, OnInit, Output, EventEmitter} from '@angular/core';
+import {Component, Input, OnInit, Output, EventEmitter, ChangeDetectionStrategy, OnChanges, SimpleChanges, ChangeDetectorRef, OnDestroy} from '@angular/core';
 import {Customer} from '../../models/customer';
 import {faTimes} from '@fortawesome/free-solid-svg-icons';
 import {FormGroup} from '@angular/forms';
 import {localIsoTime} from '../../utils/localIsoTime';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-marker-details',
   templateUrl: './marker-details.component.html',
-  styleUrls: ['./marker-details.component.scss']
+  styleUrls: ['./marker-details.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class MarkerDetailsComponent implements OnInit {
+export class MarkerDetailsComponent implements OnInit, OnDestroy  {
   @Input()
   public customer: Customer;
   @Input()
   public id: number;
   @Input('group')
   public customerInfoForm: FormGroup;
+  @Input('depot')
+  public depotForm: FormGroup;
   @Output()
   public closed = new EventEmitter<Customer>();
 
   public faTimes = faTimes;
 
-  constructor() {
+  private changeSubscription: Subscription;
+
+  constructor(private _changeDetector:ChangeDetectorRef) {
+  }
+  ngOnDestroy(): void {
+    this.changeSubscription.unsubscribe();
   }
 
   ngOnInit(): void {
     const currentDate = localIsoTime();
     if(!this.customerInfoForm.touched){
     this.customerInfoForm.patchValue({
-      dueDate: (currentDate.slice(0, 16)),
-      readyTime: (currentDate.slice(0, 16).replace("23","00")),
-      serviceTime: '00:01',
-      demand: 1
+      dueDate: (this.depotForm.controls.dueDate.value.slice(0,16)),
+      readyTime: (this.depotForm.controls.readyTime.value.slice(0,16))
     });
+
+    this.changeSubscription = this.customerInfoForm.valueChanges.subscribe(changes=>{
+       this._changeDetector.detectChanges();
+       console.log(changes);
+      });
   }
   }
 
